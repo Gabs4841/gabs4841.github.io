949baa7fae39bb677f14ebd6098c3dbd9c28ec78
Gabriel Medina da Assunção <gabriell_medina@live.com>
2023-08-08 16:45:42 -0300

    Update scrgeral.js

--- js/scrgeral.js
+++ js/scrgeral.js
@@ -4,6 +4,7 @@ function hideTablinks() {
     tablinks[i].style.display = "none";
   }
 }
+
 function toggleTablinks() {
   var tablinks = document.getElementsByClassName("tablinks");
   var show = false;
@@ -14,13 +15,10 @@ function toggleTablinks() {
     }
   }
   for (var i = 0; i < tablinks.length; i++) {
-    if (show) {
-      tablinks[i].style.display = "block";
-    } else {
-      tablinks[i].style.display = "none";
-    }
+    tablinks[i].style.display = show ? "block" : "none";
   }
 }
+
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -33,10 +31,10 @@ function openTab(evt, tabName) {
   }
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
-  display: none; 
   var hamburger = document.getElementsByClassName("hamburger")[0];
   hamburger.classList.remove("is-active");
 }
+
 function showTypingText() {
   var texts = document.querySelectorAll(".typewriter");
   texts.forEach(function(element) {
@@ -53,11 +51,13 @@ function showTypingText() {
     typeWriter();
   });
 }
+
 window.onload = function () {
   hideTablinks();
   showTypingText();
   openTab(null, 'Home');
 };
+
 var hamburger = document.getElementsByClassName("hamburger")[0];
 hamburger.addEventListener("click", function() {
   this.classList.toggle("is-active");
