168f8abc2e554175d6ffff9bd0fbc4f0bd9ed76d
Gabriel Medina da Assunção <gabriell_medina@live.com>
2023-11-10 01:32:25 -0300

    Update scrgeral.js

--- assets/JS/scrgeral.js
+++ assets/JS/scrgeral.js
@@ -1,16 +1,26 @@
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
+
 function showTypingText() {
   var texts = document.querySelectorAll(".typewriter");
   texts.forEach(function(element) {
-    var text = element.innerText;
-    element.innerText = "";
-    var index = 0;
-    function typeWriter() {
-      if (index < text.length) {
-        element.innerText += text.charAt(index);
-        index++;
-        setTimeout(typeWriter, 100);
-      }
-    }
+@@ -28,8 +14,3 @@ function showTypingText() {
     typeWriter();
   });
 }
+
+window.onload = function () {
+  showTypingText();
+  openTab(null, 'Home');
+};
